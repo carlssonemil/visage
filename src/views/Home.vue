f87a4062ab4d808977aa67217d615eb4d8e49a7b
Emil Carlsson <emil-carlsson@live.se>
2018-11-13 21:43:53 +0100

    :sparkles: Add 'Progress' component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Progress />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Progress from '@/components/Progress.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Progress
   }
 }
 </script>
