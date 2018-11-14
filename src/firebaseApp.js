bc5373d491d2f845c945b0cdf3197938808b6558
Emil Carlsson <emil-carlsson@live.se>
2018-11-14 21:26:57 +0100

    :wrench: Create dedicated Firebase config file

---
+++ src/firebaseApp.js
@@ -0,0 +1,13 @@
+import Firebase from 'firebase'
+
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+
+const FirebaseApp = Firebase.initializeApp(config);
+export const db = FirebaseApp.database();
