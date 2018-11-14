92c537bcef2dcc760ce2eae22435001596665dad
Emil Carlsson <emil-carlsson@live.se>
2018-11-14 21:27:19 +0100

    :wrench: Move Firebase to dedicated config file

--- src/views/Apply.vue
+++ src/views/Apply.vue
@@ -107,19 +107,7 @@
 </template>
 
 <script>
-import Firebase from 'firebase'
-
-const config = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
-};
-
-const app = Firebase.initializeApp(config);
-const db = app.database();
+import { db } from '../firebaseApp'
 
 let applicationsRef = db.ref("applications");
 
