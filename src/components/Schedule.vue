cbcbed9da1f4a3a215848e5d9fac6c91241a3e98
Emil Carlsson <emil-carlsson@live.se>
2018-11-14 13:43:34 +0100

    :pencil2: Change schedule times

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="schedule" class="module" data-title="Schedule">
     <ul>
-      <li><span class="weekday">Onsdag</span><span class="time">20:00-22:00</span></li>
-      <li><span class="weekday">Torsdag</span><span class="time">20:00-22:00</span></li>
-      <li><span class="weekday">Söndag</span><span class="time">20:00-22:00</span></li>
+      <li><span class="weekday">Onsdag</span><span class="time">19:45-23:00</span></li>
+      <li><span class="weekday">Torsdag</span><span class="time">19:45-23:00</span></li>
+      <li><span class="weekday">Söndag</span><span class="time">19:45-23:00</span></li>
     </ul>
   </div>
 </template>
