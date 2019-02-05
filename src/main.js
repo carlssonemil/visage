73a395004bb84eeb3a4f3e1908616815dac94b01
Emil Carlsson <emil-carlsson@live.se>
2019-02-05 21:12:19 +0100

    :sparkles: Import and use LoadScript

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import VeeValidate, { Validator } from 'vee-validate'
 import App from './App.vue'
 import router from './router'
 import Notifications from 'vue-notification'
+import LoadScript from 'vue-plugin-load-script'
 import VuePlyr from 'vue-plyr'
 import 'vue-plyr/dist/vue-plyr.css'
 
 Vue.use(VueFire)
 Vue.use(VeeValidate)
 Vue.use(Notifications)
+Vue.use(LoadScript)
 Vue.use(VuePlyr)
 
 Validator.localize('sv', sv)
